e49c6278d10189b134c5924e346020d0a63a2248
Elle Faraday <faraday.elle@gmail.com>
2017-11-21 03:50:51 +0000

    automatically backed up by learn

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -20,7 +20,19 @@ class MusicLibraryController
       puts "What would you like to do?"
 
       user_input = gets.chomp
-      if user_input = list songs
+      if user_input == 'list songs'
+        self.list_songs
+      elsif user_input == 'list artists'
+        self.list_artists
+      elsif user_input == 'list genres'
+        self.list_genres
+      elsif user_input == 'list_artist'
+        self.list_songs_by_artist
+      elsif user_input == 'list genre'
+        self.list_songs_by_genre
+      elsif user_input == 'play_song'
+        self.play_song
+      end
     end
   end
 
